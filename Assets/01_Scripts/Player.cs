44b70254b96f29ce4f287ea02180f51ff4e125b4
santiagosanchez-05 <saa0035135@est.univalle.edu>
2025-10-03 15:50:51 -0400

    Cambios

--- Assets/01_Scripts/Player.cs
+++ Assets/01_Scripts/Player.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 10f;
+    public KeyCode upKey;
+    public KeyCode downKey;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float movement = 0f;
+
+        if (Input.GetKey(upKey))
+            movement = speed * Time.deltaTime;
+        else if (Input.GetKey(downKey))
+            movement = -speed * Time.deltaTime;
+
+        transform.Translate(0, movement, 0);
+
+        // Clamp position
+        Vector3 pos = transform.position;
+        pos.y = Mathf.Clamp(pos.y, -4f, 4f);
+        transform.position = pos;
     }
 }
